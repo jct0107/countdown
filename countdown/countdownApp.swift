384f54feb07722cc079a328e948ea29e05aaf6c1
MAC <mac@MacBook-Pro.local>
2022-10-28 19:12:13 -0700

    Se agrega el proyecto

---
+++ countdown/countdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  countdownApp.swift
+//  countdown
+//
+//  Created by MAC on 28/10/22.
+//
+
+import SwiftUI
+
+@main
+struct countdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
